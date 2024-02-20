fd2e61aefcdcd81d945bbab3c68236248dfc64ab
Carlson Santana Cruz <carlsonsantana@gmail.com>
2024-02-20 19:28:44 -0300

    Validar o payload do cadastro de transação

--- src/main/java/io/github/rinhabackend2/springboot/controller/ClienteController.java
+++ src/main/java/io/github/rinhabackend2/springboot/controller/ClienteController.java
@@ -1,16 +1,22 @@
 package io.github.rinhabackend2.springboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.rinhabackend2.springboot.dto.RequestTransacaoDTO;
 import io.github.rinhabackend2.springboot.dto.ResponseExtratoDTO;
 import io.github.rinhabackend2.springboot.dto.ResponseTransacaoDTO;
 import io.github.rinhabackend2.springboot.service.ClienteService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/clientes")
@@ -23,12 +29,17 @@ public class ClienteController {
 
 	@PostMapping("/{idCliente}/transacoes")
 	public ResponseTransacaoDTO cadastrarTransacao(@PathVariable int idCliente,
-			@RequestBody RequestTransacaoDTO transacao) {
+			@Valid @RequestBody RequestTransacaoDTO transacao) {
 		return service.cadastrarTransacao(idCliente, transacao);
 	}
 
 	@GetMapping("/{idCliente}/extrato")
 	public ResponseExtratoDTO gerarExtrato(@PathVariable int idCliente) {
 		return service.gerarExtrato(idCliente);
+	}
+
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+	@ExceptionHandler({ HttpMessageNotReadableException.class, MethodArgumentNotValidException.class })
+	public void handleException(Exception ex) {
 	}
 }
--- src/main/java/io/github/rinhabackend2/springboot/dto/RequestTransacaoDTO.java
+++ src/main/java/io/github/rinhabackend2/springboot/dto/RequestTransacaoDTO.java
@@ -1,7 +1,10 @@
 package io.github.rinhabackend2.springboot.dto;
 
 import io.github.rinhabackend2.springboot.enums.TipoTransacao;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record RequestTransacaoDTO(long valor, TipoTransacao tipo, String descricao) {
-
+public record RequestTransacaoDTO(@NotNull @Min(0) Long valor, @NotNull TipoTransacao tipo, @NotEmpty @Size(min = 1, max = 10) String descricao) {
 }
